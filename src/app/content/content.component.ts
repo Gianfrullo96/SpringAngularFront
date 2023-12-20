eeac905d42df4e084aff6306a511a8e4483d712c
Ferruzzi Gianluca <gianlucaferruzzi96@gmail.com>
2023-12-20 12:09:21 +0100

    arealo

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { AxiosService } from '../axios.service';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+
+  	componentToShow: string = "login";
+
+	constructor(private axiosService: AxiosService) { }
+
+	showComponent(componentToShow: string): void {
+    this.componentToShow = componentToShow;
+  }
+
+	onLogin(input: any): void {
+		this.axiosService.request(
+		    "POST",
+		    "/login",
+		    {
+		        login: input.login,
+		        password: input.password
+		    }).then(
+		    response => {
+		        this.componentToShow = "messages";
+		    }).catch(
+		    error => {
+		       
+		        this.componentToShow = "welcome";
+		    }
+		);
+
+	}
+
+	onRegister(input: any): void {
+		this.axiosService.request(
+		    "POST",
+		    "/register",
+		    {
+		        firstName: input.firstName,
+		        lastName: input.lastName,
+		        login: input.login,
+		        password: input.password
+		    }).then(
+		    response => {
+		        this.componentToShow = "messages";
+		    }).catch(
+		    error => {
+		        
+		        this.componentToShow = "welcome";
+		    }
+		);
+	}
+
+}
+
